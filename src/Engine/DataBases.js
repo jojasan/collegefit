abd78f88c83284570c66b7a17169ad0f53a24ea1
jjenriquez27 <jjenriquez27@gmail.com>
2019-01-31 13:06:47 -0800

    Completed Table A5

--- src/Engine/DataBases.js
+++ src/Engine/DataBases.js
@@ -33,10 +33,195 @@ const tableA5 = [
     allowanceIf2Parent: 1300,
     allowanceIf1Parent: 700,
   },
-  {//ADD the rest here
-    ageOfOlderParent: 0,
-    allowanceIf2Parent: 0,
-    allowanceIf1Parent: 0,
+  {
+    ageOfOlderParent: 28,
+    allowanceIf2Parent: 2000,
+    allowanceIf1Parent: 1000,
+  },
+  {
+    ageOfOlderParent: 29,
+    allowanceIf2Parent: 2600,
+    allowanceIf1Parent: 1400,
+  },
+  {
+    ageOfOlderParent: 30,
+    allowanceIf2Parent: 3300,
+    allowanceIf1Parent: 1700,
+  },
+  {
+    ageOfOlderParent: 31,
+    allowanceIf2Parent: 4000,
+    allowanceIf1Parent: 2100,
+  },
+  {
+    ageOfOlderParent: 32,
+    allowanceIf2Parent: 4600,
+    allowanceIf1Parent: 2400,
+  },
+  {
+    ageOfOlderParent: 33,
+    allowanceIf2Parent: 5300,
+    allowanceIf1Parent: 2800,
+  },
+  {
+    ageOfOlderParent: 34,
+    allowanceIf2Parent: 5900,
+    allowanceIf1Parent: 3100,
+  },
+  {
+    ageOfOlderParent: 35,
+    allowanceIf2Parent: 6600,
+    allowanceIf1Parent: 3500,
+  },
+  {
+    ageOfOlderParent: 36,
+    allowanceIf2Parent: 7300,
+    allowanceIf1Parent: 3800,
+  },
+  {
+    ageOfOlderParent: 37,
+    allowanceIf2Parent: 7900,
+    allowanceIf1Parent: 4200,
+  },
+  {
+    ageOfOlderParent: 38,
+    allowanceIf2Parent: 8600,
+    allowanceIf1Parent: 4500,
+  },
+  {
+    ageOfOlderParent: 39,
+    allowanceIf2Parent: 9200,
+    allowanceIf1Parent: 4900,
+  },
+  {
+    ageOfOlderParent: 40,
+    allowanceIf2Parent: 9900,
+    allowanceIf1Parent: 5200,
+  },
+  {
+    ageOfOlderParent: 41,
+    allowanceIf2Parent: 10100,
+    allowanceIf1Parent: 5300,
+  },
+  {
+    ageOfOlderParent: 42,
+    allowanceIf2Parent: 10400,
+    allowanceIf1Parent: 5500,
+  },
+  {
+    ageOfOlderParent: 43,
+    allowanceIf2Parent: 10600,
+    allowanceIf1Parent: 5600,
+  },
+  {
+    ageOfOlderParent: 44,
+    allowanceIf2Parent: 10900,
+    allowanceIf1Parent: 5700,
+  },
+  {
+    ageOfOlderParent: 45,
+    allowanceIf2Parent: 11100,
+    allowanceIf1Parent: 5800,
+  },
+  {
+    ageOfOlderParent: 46,
+    allowanceIf2Parent: 11400,
+    allowanceIf1Parent: 6000,
+  },
+  {
+    ageOfOlderParent: 47,
+    allowanceIf2Parent: 11600,
+    allowanceIf1Parent: 6100,
+  },
+  {
+    ageOfOlderParent: 48,
+    allowanceIf2Parent: 11900,
+    allowanceIf1Parent: 6200,
+  },
+  {
+    ageOfOlderParent: 49,
+    allowanceIf2Parent: 12200,
+    allowanceIf1Parent: 6400,
+  },
+  {
+    ageOfOlderParent: 50,
+    allowanceIf2Parent: 12500,
+    allowanceIf1Parent: 6500,
+  },
+  {
+    ageOfOlderParent: 51,
+    allowanceIf2Parent: 12900,
+    allowanceIf1Parent: 6700,
+  },
+  {
+    ageOfOlderParent: 52,
+    allowanceIf2Parent: 13200,
+    allowanceIf1Parent: 6800,
+  },
+  {
+    ageOfOlderParent: 53,
+    allowanceIf2Parent: 13500,
+    allowanceIf1Parent: 7000,
+  },
+  {
+    ageOfOlderParent: 54,
+    allowanceIf2Parent: 13900,
+    allowanceIf1Parent: 7200,
+  },
+  {
+    ageOfOlderParent: 55,
+    allowanceIf2Parent: 14300,
+    allowanceIf1Parent: 7300,
+  },
+  {
+    ageOfOlderParent: 56,
+    allowanceIf2Parent: 14700,
+    allowanceIf1Parent: 7500,
+  },
+  {
+    ageOfOlderParent: 57,
+    allowanceIf2Parent: 15100,
+    allowanceIf1Parent: 7700,
+  },
+  {
+    ageOfOlderParent: 58,
+    allowanceIf2Parent: 15500,
+    allowanceIf1Parent: 7900,
+  },
+  {
+    ageOfOlderParent: 59,
+    allowanceIf2Parent: 15900,
+    allowanceIf1Parent: 8100,
+  },
+  {
+    ageOfOlderParent: 60,
+    allowanceIf2Parent: 16400,
+    allowanceIf1Parent: 8300,
+  },
+  {
+    ageOfOlderParent: 61,
+    allowanceIf2Parent: 16800,
+    allowanceIf1Parent: 8500,
+  },
+  {
+    ageOfOlderParent: 62,
+    allowanceIf2Parent: 17300,
+    allowanceIf1Parent: 8800,
+  },
+  {
+    ageOfOlderParent: 63,
+    allowanceIf2Parent: 17800,
+    allowanceIf1Parent: 9000,
+  },
+  {
+    ageOfOlderParent: 64,
+    allowanceIf2Parent: 18300,
+    allowanceIf1Parent: 9200,
+  },
+  {
+    ageOfOlderParent: 65,
+    allowanceIf2Parent: 18900,
+    allowanceIf1Parent: 9500,
   },
 ];
 
